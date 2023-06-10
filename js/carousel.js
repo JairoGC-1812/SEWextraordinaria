a454e60cefbbfcb65baa82c92cbf395d6c5a8ef3
JairoGC-1812 <JairoGCastro@hotmail.com>
2023-06-10 09:16:29 +0200

    Gastronomía y carrusel listos CSS completo para gastronomía y parcial para índice Carrusel funciona adecuadamente Queda adaptar fotos pa que mantengan el mismo tamaño y ahorrar movidas

---
+++ js/carousel.js
@@ -0,0 +1,41 @@
+class Carousel {
+    constructor(imgDict) {
+        if (imgDict === undefined) {
+            this.imgDict =
+            {
+                "abastos_1.jpg": "Interior del mercado de abastos de Villaviciosa",
+                "molin_darriba.jpg": "Molín d'Arriba, junto al Ríu Profundu",
+                "teatro_riera.jpg": "Fachada del Teatro Riera de Villaviciosa",
+                "villaviciosa_aerea.jpg": "Vista aérea de Villaviciosa",
+                "ateneo_pintura.jpg": "Cuadro pintado del Ateneo Obrero de Villaviciosa",
+                "exaltacion_manzana_1.jpg": "Escultura de la Exaltación a la Manzana, de Eduardo Úrculo",
+                "molin_perniles1.jpg": "Molín de Perniles, junto al Ríu Profundu",
+                "ateneo_1.jpg": "Fachada del Ateneo Obrero de Villaviciosa"
+            }
+        }
+        else {
+            this.imgDict = imgDict;
+        }
+        this.index = 0;
+    }
+
+    prev() {
+        this.index--;
+        if (this.index == -1)
+            this.index = Object.keys(this.imgDict).length - 1;
+        this.setImage();
+    }
+
+    next() {
+        this.index++;
+        if (this.index == Object.keys(this.imgDict).length)
+            this.index = 0;
+        this.setImage();
+    }
+    setImage() {
+        var img = document.querySelector("button + img");
+        img.src = "multimedia/" + Object.keys(this.imgDict)[this.index];
+        img.alt = Object.values(this.imgDict)[this.index];
+    }
+}
+var carousel = new Carousel();
